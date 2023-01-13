1c3880dddb2163ab23440ad559128e4eaed493b5
grmos <33379037+grmos@users.noreply.github.com>
2023-01-13 17:47:18 +0200

    Add files via upload

--- myneuralnetwork.py
+++ myneuralnetwork.py
@@ -14,13 +14,15 @@ ap.add_argument("-td", "--TrainingData", type=str, default="output_celebA/celebA
 	help=" training data file")
 ap.add_argument("-vd", "--ValidationData", type=str, default="output_celebA/celebA_val_Embeddings.pickle",
 	help=" validation data file")
+ap.add_argument("-bvd", "--boolValidationData", type=int, default=1,
+	help=" 0=>No validation data ,1=> Validation data")
 ap.add_argument("-ep", "--EPOCHS", type=int, required=True)
 ap.add_argument("-lr", "--learning_rate", type=float, default=0.001)
 ap.add_argument("-th_ANN", "--thresshold_ANN", type=float, default=0.95,
 	help="thresshold score for classification in ANN  ")
-ap.add_argument("-ms", "--modelsave", type=int, default=0,
+ap.add_argument("-ms", "--modelsave", type=int, default=1,
 	help=" True if you want to save the model after training")
-ap.add_argument("-mp", "--modelpath", type=str, default="pretrained/classifier.hdf5",
+ap.add_argument("-mp", "--modelpath", type=str, default="pretrained/classifier1.hdf5",
 	help=" path for saving the model after training")
 ap.add_argument("-nnh", "--numofneurons", type=int, default=64,
 	help=" number of  neurons in the hidden layer")
@@ -28,15 +30,19 @@ args = vars(ap.parse_args())
 
 ##Prepare DATA (Train , Validation)
 train_data = pickle.loads(open(os.path.abspath(args["TrainingData"]), "rb").read())
-val_data = pickle.loads(open(os.path.abspath(args["ValidationData"]), "rb").read())
-labels=train_data["names"]+val_data["names"]
+if(args["boolValidationData"]):
+    val_data = pickle.loads(open(os.path.abspath(args["ValidationData"]), "rb").read())
+    labels=train_data["names"]+val_data["names"]
+    testX=np.array(val_data["embeddings"])
+else:
+    labels=train_data["names"]
 trainX=np.array(train_data["embeddings"])
-testX=np.array(val_data["embeddings"])
 labels=np.array(labels)
 lb=LabelBinarizer()
 labels=lb.fit_transform(labels)
 trainY=labels[0:trainX.shape[0]]
-testY=labels[trainX.shape[0]:trainX.shape[0]+testX.shape[0]]
+if(args["boolValidationData"]):
+    testY=labels[trainX.shape[0]:trainX.shape[0]+testX.shape[0]]
 
 ##Construct Model archtecture
 model=tf.keras.Sequential()
@@ -45,7 +51,6 @@ model.add((tf.keras.layers.Dense(trainY.shape[1],activation="softmax")))
 
 ##Configuration
 EPOCHS=args["EPOCHS"]  
-print(EPOCHS)
 lr=args["learning_rate"]  
 th_ANN=args["thresshold_ANN"]  
 BS=len(trainX)
@@ -53,31 +58,35 @@ BS=len(trainX)
 ##Training
 opt=tf.keras.optimizers.Adam(lr, decay=lr / EPOCHS)
 model.compile(loss="categorical_crossentropy",optimizer=opt,metrics=["accuracy"])
-H=model.fit(trainX,trainY,validation_data=(testX,testY),epochs=EPOCHS,batch_size=BS)
+if(args["boolValidationData"]):
+    H=model.fit(trainX,trainY,validation_data=(testX,testY),epochs=EPOCHS,batch_size=BS)
+else:
+    H=model.fit(trainX,trainY,epochs=EPOCHS,batch_size=BS)
 if(args["modelsave"] ):
     model.save(args["modelpath"] )
 
 ##Testing
-predictions=model.predict(testX)
-error,res=0,0
-for i in range(len(testX)):
-    if(predictions.max(axis=1)[i]>=th_ANN):
-        res+=1
-        if(testY.argmax(axis=1)[i]!=predictions.argmax(axis=1)[i]):
-            error+=1
-    elif(testY.argmax(axis=1)[i]==10 and predictions.max(axis=1)[i]<0.98):
-        res+=1
-print("RESULTS WITH THRESSHOLD:{}/{}".format(res-error,res))
-if(res!=0):
-    print((1-error/res)*100)
+if(args["boolValidationData"]):
+    predictions=model.predict(testX)
+    error,res=0,0
+    for i in range(len(testX)):
+        if(predictions.max(axis=1)[i]>=th_ANN):
+            res+=1
+            if(testY.argmax(axis=1)[i]!=predictions.argmax(axis=1)[i]):
+                error+=1
+        elif(testY.argmax(axis=1)[i]==10 and predictions.max(axis=1)[i]<0.98):
+            res+=1
+    print("RESULTS WITH THRESSHOLD:{}/{}".format(res-error,res))
+    if(res!=0):
+        print((1-error/res)*100)
 
-print(classification_report(testY.argmax(axis=1),predictions.argmax(axis=1)))
-pyplot.title('Learning Curves')
-pyplot.xlabel('Epochs')
-pyplot.ylabel('Loss')
-pyplot.plot(H.history["loss"], label='train')
-pyplot.plot(H.history["val_loss"], label='val')
-pyplot.legend()
-pyplot.show()
+    print(classification_report(testY.argmax(axis=1),predictions.argmax(axis=1)))
+    pyplot.title('Learning Curves')
+    pyplot.xlabel('Epochs')
+    pyplot.ylabel('Loss')
+    pyplot.plot(H.history["loss"], label='train')
+    pyplot.plot(H.history["val_loss"], label='val')
+    pyplot.legend()
+    pyplot.show()
 
 
